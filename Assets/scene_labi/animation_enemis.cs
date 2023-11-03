1a18bf05f4e2bf7f1e581cb06e33b28b9d413ef0
FloTurbo <thedronestarwars@gmail.com>
2023-11-03 14:26:27 -0400

    modif Vendredi

--- Assets/scene_labi/animation_enemis.cs
+++ Assets/scene_labi/animation_enemis.cs
@@ -6,14 +6,33 @@ public class animation_enemis : MonoBehaviour
 {
     // Start is called before the first frame update
     public float vitesse = 1f; /* vitesse des monstres */
-    void Start()
-    {
-        
-    }
+    public GameObject[] points;
+   
+    int indexActuel = 0;
+
+    ////méthode getDistance
+    //private float getDistance()
+    //{
+    //    return Vector3.Distance(transform.position, points[indexActuel].transform.position);
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(transform.position, points[indexActuel].transform.position) < .1f)
+        {
+            //inbcrémentation de l'indez
+            indexActuel++;
+
+            if (indexActuel >= points.Length)
+            {
+
+                indexActuel = 0;
+            }
+
+        }
+
+        //déplace l'objet
+        transform.position = Vector3.MoveTowards(transform.position, points[indexActuel].transform.position, vitesse * Time.deltaTime);
     }
 }
